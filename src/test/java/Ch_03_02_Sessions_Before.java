9ba5e23a7733bd98cc690977841f4ee9983af150
Teddy Nzioka <nziokateddy@gmail.com>
2021-05-19 00:29:51 +0300

    Initial commit

---
+++ src/test/java/Ch_03_02_Sessions_Before.java
@@ -0,0 +1,48 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class Ch_03_02_Sessions_Before {
+
+    private static final String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.apk";
+    private static final String APPIUM = "http://localhost:4723/wd/hub";
+
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "10");
+        caps.setCapability("deviceName", "Android Emulator");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("app", APP);
+        driver = new AndroidDriver(new URL(APPIUM), caps);
+
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Login Screen")));
+
+    }
+}
